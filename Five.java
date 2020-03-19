37f9c4a4a88f6f471cab6c2c1e996131f77ec505
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-19 22:26:47 +0530

    Add files via upload

---
+++ Five.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Five 
+{
+    
+public static void main(String[ ] args) {
+        
+int a,b,c;
+        
+double d;
+        
+double p,q;
+        
+Scanner x=new Scanner(System.in);
+        a=x.nextInt();
+        
+b=x.nextInt();
+        
+c=x.nextInt();
+        
+d=b*b-4*a*c;
+        
+if(d>=0)
+        
+{
+            
+p=(-b+Math.sqrt(d))/(2*a);
+            
+q=(-b-Math.sqrt(d))/(2*a);
+            
+System.out.println("root1="+p+" "+"root2="+q);
+
+        
+}
+        
+else
+        
+System.out.println("roots are imaginary");
+    }
+}
