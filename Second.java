f1200e7862fa9b33e9e340a52d0e13fcfd083fdc
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-19 21:42:19 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,27 @@
+// Variable, Data types 
+import java.util.Scanner;
+
+class second
+{
+   
+ public static void main(String[ ] args)
+{
+        
+int r;
+        
+float pi=3.14f;
+        
+float A;
+        
+Scanner s=new Scanner(System.in);
+        
+r=s.nextInt();
+       
+ A=pi*r*r;
+        
+System.out.println("Area="+A);
+        
+    
+}
+
+}
