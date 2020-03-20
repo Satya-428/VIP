2626d68f9f4f96e38cbec539590c14d64deb48e0
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-20 15:56:31 +0530

    Add files via upload

---
+++ sixteen.java
@@ -0,0 +1,11 @@
+// Logical Operator example
+class Sixteen {
+   public static void main(String args[]) {
+      boolean n = true;
+      boolean m = false;
+
+      System.out.println("n && m: " + (n&&m));
+      System.out.println("n || m: " + (n||m));
+      System.out.println("!(n && m): " + !(n&&m));
+   }
+}
