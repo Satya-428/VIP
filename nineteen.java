759a06fc7d837cbba3c721fc6156b3eba95387cc
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-20 18:35:14 +0530

    Add files via upload

---
+++ nineteen.java
@@ -0,0 +1,9 @@
+// ASCII value
+public class Nineteen {
+	public static void main(String[] args) {
+		char ch = 'a';
+	        	int a = ch;
+	        	int cascii = (int) ch;
+		System.out.println("The ASCII value of " + ch + " is: " + a);
+		}
+}
