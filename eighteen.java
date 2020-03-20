02ad161b44ec19bb83d22f0e90f1323063c03125
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-20 18:07:04 +0530

    Add files via upload

---
+++ eighteen.java
@@ -0,0 +1,25 @@
+// Pre/Post increment and decrement
+import java.util.*;
+
+public class Program
+{
+	
+public static void main(String[] args) {
+	    
+int a,b;
+	    
+Scanner s=new Scanner(System.in);
+	    
+a=s.nextInt();
+	    
+b=s.nextInt();
+	    
+System.out.println(++a); /* pre-increment-> first displays the output then displays the initial value of the variable*/
+	    System.out.println(a++); /* post-increment -> first displays the current value of the variable and then dispalys the output value */
+	    System.out.println(b--); // similar to post-increment
+	    
+System.out.println(--b); // similar  to pre-increment
+	
+}
+
+}
