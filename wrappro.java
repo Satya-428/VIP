893ee21df0aabfb333ac34df5349f76809b47784
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-21 13:39:32 +0530

    Add files via upload

---
+++ wrappro.java
@@ -0,0 +1,12 @@
+// Wrapper class
+import java.util.Scanner;
+public class Wrappro {
+		public static void main(String[] args) 
+		{
+		    Scanner s=new Scanner(System.in);
+		    Integer iObj= new Integer(5);
+		    System.out.println(iObj);
+		}
+	}
+
+
