652840a85874ebc24c17842f853046dc9c54c7aa
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-21 13:17:35 +0530

    Add files via upload

---
+++ unbox.java
@@ -0,0 +1,20 @@
+// Unboxing
+import java.util.ArrayList; 
+public class Unbox {
+
+	    public static void main(String args[])
+	    { 
+	        Character ch = 'a'; 
+	  
+	        char a = ch; 
+	  
+	        ArrayList<Integer> arrayList = new ArrayList<Integer>(); 
+	        arrayList.add(42); 
+	  
+	        int num = arrayList.get(0); 
+	   
+	        System.out.println(num); 
+
+	    } 
+	} 
+
