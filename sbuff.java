c0c64c694a8bb2b723042910e78da3a0f5062d43
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-21 12:54:01 +0530

    Add files via upload

---
+++ sbuff.java
@@ -0,0 +1,10 @@
+// String buffer 
+public class Sbufferpro {
+		public static void main(String[] args) {
+		    StringBuffer sbf= new StringBuffer("MASTEK ");
+		    sbf.append("EMPLOYEE");
+		    
+		    System.out.println(sbf);
+			
+		}
+}
