6104ac1f6aac8405355e4d0ecc91e5fc7ea3f626
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-21 13:24:12 +0530

    Add files via upload

---
+++ autobox.java
@@ -0,0 +1,21 @@
+// Autobox
+import java.util.ArrayList; 
+
+public class Autobox {
+
+	    public static void main(String[] args) 
+	    { 
+	        char ch = 'a'; 
+	  
+	        Character a = ch; 
+	  
+	        ArrayList<Integer> arrayList = new ArrayList<Integer>(); 
+	  
+
+	        arrayList.add(42); 
+	  
+	    
+	        System.out.println(arrayList.get(0)); 
+	    } 
+	} 
+
