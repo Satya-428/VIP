978b64531547121602fddc315c9369016e585626
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-22 19:30:43 +0530

    Add files via upload

---
+++ pattertwo.java
@@ -0,0 +1,18 @@
+// Equilateral triangle
+import java.util.Scanner;
+public class Patterntwo {
+	    public static void main(String[] args) {
+	    int i,j,n;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    for(i=1;i<=n;i++){
+	        for(j=1;j<=i;j++){
+	            System.out.print("*"+" ");
+	        }
+	        System.out.println();
+	    }
+	       
+	    }
+	}
+
+
