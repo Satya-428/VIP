594eef0409e3ed57790cea9640b6147d16b16468
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-22 19:24:35 +0530

    Add files via upload

---
+++ patternone.java
@@ -0,0 +1,19 @@
+// Box pattern
+import java.util.Scanner;
+public class Patternone {
+	    public static void main(String[] args) {
+	    int i,j,n,m;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    for(i=1;i<=n;i++){
+	        for(j=0;j<=n;j++){
+	            System.out.print("*"+" ");
+	        }
+	        System.out.println();
+	    }
+	    
+	        
+	    }
+	}
+
+
