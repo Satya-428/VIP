2a292eee3462081bff9ad081973bbefdef4bcc2f
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-22 19:05:58 +0530

    Add files via upload

---
+++ abstpro.java
@@ -0,0 +1,27 @@
+// Method overriding
+	abstract class Animal {
+		   public abstract void makeSound();
+		}
+
+		class Dog extends Animal {
+		   public void makeSound() {
+		      System.out.println("Bark bark..");
+		   }
+		}
+
+		class Cat extends Animal {
+		   public void makeSound() {
+		      System.out.println("Meow meow..");
+		   }
+		}
+
+		class Abstpro {
+		   public static void main(String[] args) {
+		     Dog  d1 = new Dog();
+		      d1.makeSound();
+
+		      Cat c1 = new Cat();
+		      c1.makeSound();
+		   }
+		}
+
