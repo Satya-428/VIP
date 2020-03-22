f447542bfc8165640e9b7bfbceee14e00afd6a3d
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-03-22 19:49:19 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,24 @@
+// Pattern
+import java.util.Scanner;
+
+public class Patternsix {
+		public static void main(String[] args) {
+		    Scanner s=new Scanner(System.in);
+		    int i,j;
+		    int n=s.nextInt();
+		    for(i=1;i<=n;i++){
+		        for(j=1;j<=i;j++){
+		            System.out.print(j+" ");
+		        }
+		        System.out.println();
+		    }
+		    for(i=n-1;i>=1;i--){
+		        for(j=1;j<=i;j++){
+		            System.out.print(j+" ");
+		        }
+		        System.out.println();
+		    }
+			
+		}
+	}
+
