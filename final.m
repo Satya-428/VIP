52493ac582b34b917160c29164f527ef70e01ed7
Satya-428 <62066661+Satya-428@users.noreply.github.com>
2020-08-20 12:45:00 +0530

    Add files via upload

---
+++ final.m
@@ -0,0 +1,185 @@
+function varargout = final(varargin)
+% FINAL MATLAB code for final.fig
+%      FINAL, by itself, creates a new FINAL or raises the existing
+%      singleton*.
+%
+%      H = FINAL returns the handle to a new FINAL or the handle to
+%      the existing singleton*.
+%
+%      FINAL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FINAL.M with the given input arguments.
+%
+%      FINAL('Property','Value',...) creates a new FINAL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before final_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to final_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help final
+
+% Last Modified by GUIDE v2.5 04-Mar-2020 12:05:52
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @final_OpeningFcn, ...
+                   'gui_OutputFcn',  @final_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before final is made visible.
+function final_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to final (see VARARGIN)
+
+% Choose default command line output for final
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes final wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = final_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+%% Diabetes Detection based on Iris Image 
+
+%% Read Test Image
+
+[filename,pathname] = uigetfile('*.jpg;*.tif;*.png;*.jpeg;*.bmp;*.pgm;*.gif','pick an imgae');
+file = fullfile(pathname,filename);
+
+   F = imread(file);
+   F = imresize(F,[280 320]);
+   axes(handles.axes1);
+   imshow(F);
+   title('Test Image');
+   
+    if size(F,3) == 3
+    F=rgb2gray(F);
+    end
+    axes(handles.axes2);
+    imshow(F);
+    title('Gray Image');
+    
+    % DCT Transformation 
+    
+F=dct(F(:));
+F=idct(F);
+F=reshape(F,280,320);
+axes(handles.axes2);
+imshow(uint8(F));
+title('DCT Transformation Result');
+
+handles.F=F;
+handles.file=file;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+F=handles.F;
+file=handles.file;
+
+%% Iris Detection
+
+radial_res = 80;
+angular_res = 240;
+
+nscales=1;
+minWaveLength=18;
+mult=1; 
+sigmaOnf=0.5;
+
+eyeimage = double(F);
+
+addpath('Segmentation');
+
+[circleiris circlepupil imagewithnoise] = segmentiris(eyeimage);
+   
+imagewithcircles = uint8(eyeimage);
+
+%get pixel coords for circle around iris
+
+[x,y] = circlecoords([circleiris(2),circleiris(1)],circleiris(3),size(eyeimage));
+ind2 = sub2ind(size(eyeimage),double(y),double(x)); 
+
+%get pixel coords for circle around pupil
+
+[xp,yp] = circlecoords([circlepupil(2),circlepupil(1)],circlepupil(3),size(eyeimage));
+ind1 = sub2ind(size(eyeimage),double(yp),double(xp));
+
+imagewithcircles(ind2) = 255;
+imagewithcircles(ind1) = 255;
+
+axes(handles.axes3);
+imshow(imagewithcircles);
+title('Iris Segmentation Result');
+
+eyeimage_filename=file;
+
+%% Iris Normaliation
+
+addpath('Normal_encoding');
+[polar_array noise_array] = normaliseiris(imagewithnoise, circleiris(2),...
+    circleiris(1), circleiris(3), circlepupil(2), circlepupil(1), circlepupil(3),eyeimage_filename, radial_res, angular_res);
+
+axes(handles.axes4);
+imshow(polar_array);
+title('Iris Normalization Result');
+
+save('NR.mat','polar_array','F');
+
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+final1
